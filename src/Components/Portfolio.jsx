528fac4edeeecf1bc2ac7b88502c35e673f33715
abdul Razzaq <ar8937562@gmail.com>
2023-09-19 07:54:07 -0700

    changes in images

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "..\images\network.png";
+import image from "../images/network.png";
 
 const imageAltText = "desktop with books and laptop";
 
