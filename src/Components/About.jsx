04cc744e59c2ee2b24a561b550d99b91bcbeea65
abdul Razzaq <ar8937562@gmail.com>
2023-09-18 21:47:15 -0700

    updated about and portfolio section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am Flutter developer and software student at Mehran University Jamshoro. I have almost 1.5 years of experience. I am currently working on different courses on Youtube.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am creating new ui designs through flutter and apart from this I also worked on front end development in 2021. I have little bit experience in front end development too. I am very passionate about technology and creating real world solutions";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,29 +31,18 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Book Store ui design",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "created basic and a beautiful ui design of books by flutter and i have added some of well known books in this project",
+    url: "https://github.com/razzaq-99/Book_store_ui",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Exploring Laptops by Expansion Tile",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Created this basic laptop ui design in which we explore different laptops by expansion tile widget in flutter. this ui design contains 5 to 7 laptops",
+    url: "https://youtu.be/WkuRVEMpn1k?si=5mvylv_fCkaMDAGc",
   },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+  
 ];
 
 const Portfolio = () => {
